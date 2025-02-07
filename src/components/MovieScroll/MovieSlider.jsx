97ad6a94904741f455c4c1b7eae7b0ddeceeac04
Shahzod <shahzodtashev49@gmail.com>
2025-02-07 20:22:16 +0500

    .

--- src/components/MovieScroll/MovieSlider.jsx
+++ src/components/MovieScroll/MovieSlider.jsx
@@ -1,68 +1,88 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { Navigation, Mousewheel, FreeMode } from 'swiper/modules';
 import styles from './MovieSlider.module.css';
+import 'swiper/css/navigation'; 
 import MovieCard from '../MovieCard/MovieCard';
 import Container from '../Container/Container';
-import { SlArrowRight, SlArrowLeft } from 'react-icons/sl';
+
+import { SlArrowRight } from 'react-icons/sl';
+import { SlArrowLeft } from 'react-icons/sl';
 import TopRatedCard from '../TopRatedCard/TopRatedCard';
 
 const MovieSlider = ({ movies, topRated }) => {
     const [isBeginning, setIsBeginning] = useState(true);
     const [isEnd, setIsEnd] = useState(false);
-    const swiperRef = useRef(null);
+
     const prevButtonRef = useRef(null);
     const nextButtonRef = useRef(null);
 
-    useEffect(() => {
-        if (!swiperRef.current) return;
-        const swiper = swiperRef.current.swiper;
-
-        const updateButtons = () => {
-            setIsBeginning(swiper.isBeginning);
-            setIsEnd(swiper.isEnd);
-        };
-
-        swiper.params.navigation.prevEl = prevButtonRef.current;
-        swiper.params.navigation.nextEl = nextButtonRef.current;
-        swiper.navigation.init();
-        swiper.navigation.update();
-        updateButtons();
-
-        swiper.on('slideChange', updateButtons);
+    const handleSlideChange = useCallback((swiper) => {
+        setIsBeginning(swiper.isBeginning || swiper.progress <= 0);
+        setIsEnd(swiper.isEnd || swiper.progress >= 1);
     }, []);
 
     return (
         <div className={styles.movieSlider}>
             <Container>
                 <Swiper
-                    ref={swiperRef}
                     slidesPerView={'auto'}
                     spaceBetween={10}
                     modules={[Mousewheel, Navigation, FreeMode]}
-                    freeMode={true}
-                    mousewheel={{ forceToAxis: true }}
-                    navigation={{ prevEl: prevButtonRef.current, nextEl: nextButtonRef.current }}
+                    freeMode={{
+                        enabled: true,
+                        momentum: true,
+                    }}
+                    mousewheel={{
+                        enabled: true,
+                        forceToAxis: true,
+                        releaseOnEdges: false,
+                    }}
+                    navigation={{
+                        nextEl: nextButtonRef.current,
+                        prevEl: prevButtonRef.current,
+                    }}
+                    onSlideChange={handleSlideChange}
+                    onSetTranslate={handleSlideChange}
+                    onInit={(swiper) => {
+                        swiper.params.navigation.prevEl = prevButtonRef.current;
+                        swiper.params.navigation.nextEl = nextButtonRef.current;
+                        swiper.navigation.init();
+                        swiper.navigation.update();
+
+                        handleSlideChange(swiper);
+                    }}
                     className={styles.swiper}
                 >
                     {movies?.map((movie, index) => (
                         <SwiperSlide key={movie.id} className={styles.movieItem}>
-                            {topRated ? (
-                                <TopRatedCard top={index + 1} posterPath={movie.poster_path} />
-                            ) : (
-                                <MovieCard posterPath={movie.poster_path} title={movie.title || movie.name} />
-                            )}
+                            {
+                                topRated ? 
+                                    <TopRatedCard top={(index + 1)} posterPath={movie.poster_path}
+                                    /> 
+                                :
+                                    <MovieCard
+                                        posterPath={movie.poster_path}
+                                        title={movie.title || movie.name}
+                                    />
+                            }
                         </SwiperSlide>
                     ))}
                 </Swiper>
             </Container>
 
-            <div ref={prevButtonRef} className={`${styles.buttonPrev} ${isBeginning ? styles.btnDisabled : ''}`}>
+            <div
+                ref={prevButtonRef}
+                className={`${styles.buttonPrev} ${isBeginning ? styles.btnDisabled : ''}`}
+            >
                 <SlArrowLeft className={styles.btn} />
             </div>
-            <div ref={nextButtonRef} className={`${styles.buttonNext} ${isEnd ? styles.btnDisabled : ''}`}>
+            <div
+                ref={nextButtonRef}
+                className={`${styles.buttonNext} ${isEnd ? styles.btnDisabled : ''}`}
+            >
                 <SlArrowRight className={styles.btn} />
             </div>
         </div>
